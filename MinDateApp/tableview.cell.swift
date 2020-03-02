80eb6c4202de95327f239bc8581884f62dcd31a2
ikemoti <ikepon413@icloud.com>
2020-03-02 18:58:43 +0900

    cell　のxib 作成

---
+++ MinDateApp/tableview.cell.swift
@@ -0,0 +1,24 @@
+//
+//  tableview.cell.swift
+//  MinDateApp
+//
+//  Created by USER on 2020/03/02.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+import UIKit
+
+class TVCell:UITableViewCell{
+    
+    
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    
+    @IBOutlet weak var cellDateName: UILabel!
+    
+    
+    
+    @IBOutlet weak var celluserName: UILabel!
+    
+}
